ba1d5f4febaffaacaab1d8cca190134fdf405b15
Marcus Ng <marcuslistanng@gmail.com>
2017-10-17 18:34:03 -0400

    Display students and averages

--- stu_mean.py
+++ stu_mean.py
@@ -7,14 +7,48 @@ db = sqlite3.connect(f)
 c = db.cursor()
 
 
-command = "SELECT students.id, name, mark FROM students,courses WHERE students.id = courses.id;"
-students = c.execute(command)
+getStudentGrades = "SELECT students.id, name, mark FROM students, courses WHERE students.id = courses.id;"
+grades = c.execute(getStudentGrades)
 
-command = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average NUMERICAL);"
-#c.execute(command)
+createPeepTable = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average NUMERICAL);"
+#c.execute(createPeepTable)
 
-for student in students:
-    print student[0]
+# Grade dictionary<id, mark>
+studentGrades = {}
+
+# Populate grade dict
+def populateStudentGrades():
+    for student in grades:
+        id = student[0];
+        mark = student[2];
+    
+        if id in studentGrades:
+            studentGrades[id].append(mark)
+        else:
+            studentGrades[id] = []
+            studentGrades[id].append(mark)
+            
+# Look through grade dictionary, while replacing array of grades with average
+def gradesAvg():
+    for studentId in studentGrades:
+        total = 0
+        for grade in studentGrades[studentId]:
+            total += grade
+        studentGrades[studentId] = float(total) / (len(studentGrades[studentId])) # Calculate average
+
+def getStudentName(uid):
+    getStudentsName = "SELECT name FROM students WHERE %d = id;" % (uid)
+    name = c.execute(getStudentsName)
+    for n in name:
+        return  n[0]
 
 
+def displayStudents():
+    for studentId in studentGrades:
+        name = getStudentName(studentId)
+        print "Student Id: %d, Name: %s, Average: %s" % (studentId, name, studentGrades[studentId])
 
+    
+populateStudentGrades()
+gradesAvg()
+displayStudents()
